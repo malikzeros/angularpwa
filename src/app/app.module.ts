98bc511c813c0c2c180dbb0974beefceb02befac
ABD. MALIK <malikzeros@gmail.com>
2020-04-22 01:53:38 +0700

    template pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TeamComponent } from './team/team.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    AboutComponent,
+    ServicesComponent,
+    FeaturedComponent,
+    TestimonialsComponent,
+    PortfolioComponent,
+    TeamComponent,
+    PricingComponent,
+    FaqComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
